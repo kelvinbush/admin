a4c57ee3ea83bafadf54cd4fb3b297efecd64624
Kelvin Wachiye <kelybush@gmail.com>
2025-02-26 16:27:20 +0300

    set up business header

--- components/business-profile/business-profile.tsx
+++ components/business-profile/business-profile.tsx
@@ -6,6 +6,9 @@ import { usePathname } from "next/navigation";
 import { useAppDispatch } from "@/lib/redux/hooks";
 import { setTitle } from "@/lib/redux/features/top-bar.slice";
 import { cn } from "@/lib/utils";
+import { useGetBusinessProfileByPersonalGuidQuery } from "@/lib/redux/services/user";
+import { Loader2 } from "lucide-react";
+import BusinessProfileHeader from "@/components/business-profile/business-profile.header";
 
 interface BusinessProfileProps {
   personalGuid: string;
@@ -42,19 +45,50 @@ const BusinessProfile = ({ personalGuid, children }: BusinessProfileProps) => {
   ];
 
   const dispatch = useAppDispatch();
-  console.log({ personalGuid });
   const pathname = usePathname();
+  const { data: response, isLoading } =
+    useGetBusinessProfileByPersonalGuidQuery(
+      { guid: personalGuid || "" },
+      { skip: !personalGuid },
+    );
 
   dispatch(setTitle("Business Profile"));
 
-  // Get the first part of the path after /[userId]/
+  // Get the first part of the path after /business-profile/
   const currentMainTab = pathname.split("/").slice(3)[0] ?? "company-details";
+
+  if (isLoading) {
+    return (
+      <div className="flex h-[50vh] items-center justify-center">
+        <Loader2 className="h-8 w-8 animate-spin text-primary" />
+      </div>
+    );
+  }
+
+  if (!response?.business) {
+    return (
+      <div className="flex h-[50vh] items-center justify-center">
+        <p className="text-destructive">Failed to load business profile</p>
+      </div>
+    );
+  }
+
+  const business = {
+    name: response.business.businessName,
+    isVerified: false,
+    completion: 80,
+    pitchDeckUrl: "https://google.com",
+    imageUrl: response.business?.businessLogo || "",
+    companyType: response.business.typeOfIncorporation,
+    location: `${response.business.city}, ${response.business.country}`,
+  };
 
   return (
     <div className="space-y-6">
-      {/*<BusinessProfileHeader*/}
-      {/*  onImageUpload={() => console.log("Image Uploaded")}*/}
-      {/*/>*/}
+      <BusinessProfileHeader
+        onImageUpload={() => console.log("Image Uploaded")}
+        {...business}
+      />
       <main className="space-y-6 bg-white">
         <Tabs value={currentMainTab} className="w-full">
           <TabsList className="w-full bg-transparent grid grid-cols-4">
