6a21c373808ff85c80652bcea705026c2ce95604
kelybush <kelybush@gmail.com>
2025-04-24 02:19:26 +0300

    set up loan products empty state

--- app/(dashboard)/_components/topnav.tsx
+++ app/(dashboard)/_components/topnav.tsx
@@ -5,7 +5,7 @@ import UserNav from "./user-nav";
 const Topnav = () => {
   const title = useAppSelector(selectTopBar);
   return (
-    <div className="sticky top-0 z-10 flex items-center justify-between bg-white py-3 pl-[308px] pr-4 shadow-md">
+    <div className="sticky top-0 z-10 flex items-center justify-between bg-white py-3 pl-[308px] pr-4">
       <p className={"text-xl font-medium leading-tight text-midnight-blue"}>
         {title}
       </p>
---
+++ app/(dashboard)/loan-productss/_components/empty-state.tsx
@@ -0,0 +1,58 @@
+import { Icons } from "@/components/icons";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
+import { Plus } from "lucide-react";
+
+interface EmptyStateProps {
+  onCreateClick?: () => void;
+}
+
+const EmptyState = ({ onCreateClick }: EmptyStateProps) => {
+  return (
+    <div className="flex flex-col items-center justify-center py-12 flex-1">
+      <Icons.noLoanProductIcon className="mb-6 h-44 w-auto" />
+      <p className="mb-6 text-center text-gray-600">
+        No loan products have been added yet!
+      </p>
+      <div className="flex flex-col gap-2">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              className="flex items-center gap-2 rounded-[4px] bg-gradient-to-r from-[#0C9] to-[#F0459C] text-white hover:from-[#0C9] hover:to-[#F0459C]"
+              size="lg"
+            >
+              <span>New Loan Product</span>
+              <Plus />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="center" className="w-56">
+            <DropdownMenuItem asChild>
+              <Link
+                href="/loan-productss/create?type=mk"
+                className="flex w-full cursor-pointer"
+              >
+                MK Loan Product
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <Link
+                href="/loan-productss/create?type=partner"
+                className="flex w-full cursor-pointer"
+              >
+                Partner Loan Product
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  );
+};
+
+export default EmptyState;
